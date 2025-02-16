9699f222c328a68763cdfcbcd78d177c85e8d5c8
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2025-02-15 17:34:28 -0800

    add category, checkUserSession, sign in, sign up, and sign out sagas

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,9 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger'; 
-import { thunk } from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
@@ -10,12 +12,15 @@ const persistConfig = {
     key: 'root', 
     storage, 
     whitelist: ['cart']
-}
+};
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger, 
-    thunk
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger, 
+    sagaMiddleware
 ].filter(Boolean);
 
 const composeEnhancer = 
@@ -31,6 +36,8 @@ export const store = createStore(
     undefined, 
     composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
